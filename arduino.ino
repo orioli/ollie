8a921bc216b09de1a5cc06dcc1c2088063dc97c3
Jose Berengueres <josetokyo@gmail.com>
2015-02-14 02:00:07 +0400

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -16,6 +16,7 @@
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);           // select the pins used on the LCD panel
 
+
 // define some values used by the panel and buttons
 int lcd_key     = 0;
 int adc_key_in  = 0;
@@ -27,34 +28,66 @@ int adc_key_in  = 0;
 #define btnSELECT 4
 #define btnNONE   5
 
-#define RELAY_HORN   2   // horn relay
-#define RELAY_BEEP   11  // beeper 
+#define RELAY_HORN    11   // horn relay
+#define RELAY_BEEP   2  // beeper 
 #define STD_DELAY 300
 
 #define SHORT_BEEP_LENGTH 500
 
-long prev_s;
+#define WARNING_BEEP 0
+#define SHORT_HORN 1
+#define LONG_HORN 2
+#define SUPER_LONG_HORN 3
+
+unsigned long  len_of_note[] = {500,400,1000,2000}; // in ms
+
+int prev_s;
 
 int prev_mode = 5;
 int mode = 5;
-unsigned long start;
-unsigned long ctdwn;
-unsigned long sound_start;
+long start;
+long ctdwn;
+long sound_start;
 
-unsigned long  *sch;
+unsigned long *sch;
 int *h_or_b;
 int index;
+//                          warning long buzz (sb)= 0,   short horn (sh) = 1, long horn (lh) =2  extralong horn 3
+// Short seq countdown // 10lb  5sh   5blank  3lh
 
-// Short seq countdown
-unsigned long  sch_3[] =   { 0, 1 ,2, 3, 4, 5, 10, 19, 20, 28, 29, 30, 1*60, 2*60, 2*60+58, 2*60+59, 3*60, 3*60+1, 3*60+2, 3*60+3  };
-int h_or_b3[] =            { 0, 1, 1, 1, 1, 1,  1,  1,  1,  1,  1,  1,    0,    0,       0,       0,    0,      1,      1,      1  };
-int index_3 = 19;
+unsigned long sch_3[] =   {    0,   10,20,30,40,50, 60,70,80,90,100,  
+
+                               190, 200, 
+                               280,290,300, 
+                            
+                               1*600, 600 +10, 600+20, 600+30, 600+40, 600+50,
+                            
+                               2*600-15, 2*600, 2*600+10, 2*600+20, 2*600+30, 2*600+40, 2*600+50,
+                          
+                               3*600-30, 3*600-15, 3*600, 3*600+60, 3*600+70,3*600+80,3*600+90,3*600+100,3*600+110, 3*600+120, 3*600+130, 3*600+140, 3*600+150, 3*600+160,3*600+170, 3*600+180, 3*600+190, 3*600+200  };
+
+int h_or_b3[] =            {   3,   2,2,2,2,2,   0,0,0,0,0, 
+                               1,1,
+                               1,1,1,
+                               2, 0,0,0,0,0,
+                               2,2,0,0,0,0,0,
+                               2,2,2,1,1,1,1,1,0, 0, 0, 0, 0, 0, 0, 0, 0, 0  };
+int index_3 = 46;
 
 
-// SLong seq countdown
-unsigned long  sch_5[] =   { 0, 1 ,2, 3, 4, 5, 10, 19, 20, 28, 29, 30, 1*60, 4*60, 4*60+56, 4*60+57, 4*60+58, 4*60+59, 5*60, 5*60+1, 5*60+2, 5*60+3, 5*60+4, 5*60+5  };
-int h_or_b5[] =            { 0, 1, 1, 1, 1, 1,  1,  1,  1,  1,  1,  1,    0,    0,       0,       0,       0,     0,    0,      1,      1,      1,      1,      1    };
-int index_5 = 23;
+unsigned long sch_5[] =   {    0,   10,20,30,40, 
+
+                               1*600, 1*600+10, 1*600+20, 1*600+30, 
+                            
+                               4*600, 4*600+10, 4*600+20, 4*600+30, 
+                          
+                               5*600, 5*600+60, 5*600+70,5*600+80,5*600+90,5*600+100,5*600+110, 5*600+120, 5*600+130, 5*600+140, 5*600+150, 5*600+160,5*600+170, 5*600+180, 5*600+190, 5*600+200  };
+
+int h_or_b5[] =            {   3, 2, 0,0,0, 
+                               2, 0,0,0,
+                               2,0,0,0,
+                               2,1,1,1,1,1,0, 0, 0, 0, 0, 0, 0, 0, 0, 0  };
+int index_5 = 28;
 
 bool sound_on = false;
 
@@ -103,7 +136,7 @@ void lcd_w(char a[16],char b[16]) {
 }
 
 void mymenu(){
-    lcd_w("OLLIE BOX v 2.1 ","BY J BERENGUERES");
+    lcd_w("BY CHIRS LABORDE"," & J BERENGUERES");
     lcd_w("  INSTRUCTIONS  ","                ");
     lcd_w(" 'LEFT' --> BUZZ","'RIGHT' --> HORN");
     lcd_w("TO STOP SEQUENCE","  --> 'SELECT'  ");
@@ -122,6 +155,8 @@ void vibrate(int n,int b, int c) {
 */
 
 void setup(){
+   Serial.begin(9600);
+   Serial.println("hello");
    pinMode(RELAY_HORN, OUTPUT);
    pinMode(RELAY_BEEP, OUTPUT);
    lcd.begin(16, 2);
@@ -129,25 +164,27 @@ void setup(){
    mymenu();
    lcd.setCursor(0,1);            
    lcd.print("00:00 ");
+   
+    
 }
 
 void activate_sound(int a) {
+  // check what instrument to sound
       int what_beep = RELAY_HORN;
-      if (a==1) { what_beep = RELAY_BEEP;}
+      if (a==WARNING_BEEP) { what_beep = RELAY_BEEP;}
       digitalWrite(what_beep, HIGH);
       sound_start = millis();
       lcd.setCursor(7,1);
-      if (a==0)  {    
-            lcd.print("H");
-      }else{
+      if (a==WARNING_BEEP)  {    
             lcd.print("B");
-      
+      }else{
+            lcd.print("H");
       }
 }
 
 void de_activate_sound(int a) {
       int what_beep = RELAY_HORN;
-      if (a==1) { what_beep = RELAY_BEEP;}
+      if (a==WARNING_BEEP) { what_beep = RELAY_BEEP;}
       digitalWrite(what_beep, LOW);
       lcd.setCursor(7,1);
       lcd.print("        ");
@@ -155,16 +192,23 @@ void de_activate_sound(int a) {
 
 void horn_or_beep(long s){
     if (sound_on) {
-         if ( ((millis()-sound_start) > SHORT_BEEP_LENGTH) ) {
+         if ( ((millis()-sound_start) > len_of_note[h_or_b[index]] ) ) {
             de_activate_sound(h_or_b[index]);
             sound_on = false;
             index = index - 1;
          }
     }else{
-      if ( s < (sch[index]*1000 + 500) ) {
-           activate_sound(h_or_b[index]);
-           sound_on = true;
-      }
+         unsigned long v = (sch[index]*100);
+         //Serial.println("long v = (long) (sch[index]*1000);");
+         //Serial.println(index);
+         //Serial.println(s);
+         //Serial.println(v);
+         
+
+         if ( s < v + 500  ) {
+             activate_sound(h_or_b[index]);
+             sound_on = true;
+         }
     }
 }
 
@@ -199,7 +243,7 @@ void diplay_timer(long s){
 void loop(){
    long s =  millis()-start ;    
    if (mode == btnUP || mode == btnDOWN ) {
-    long d = ctdwn*1000 - s ;
+     long d = ctdwn*1000 - s ;
      horn_or_beep( d );
      diplay_timer( d ); 
    }
@@ -243,11 +287,11 @@ void loop(){
              lcd.setCursor(0,1);            
              lcd.print("00:00");
              mode = btnUP;
-             ctdwn = 5*60 + 5 + 1;
+             ctdwn = 300+25 ; //index_3-1 
              sound_on = false;
              sch = sch_5;
              h_or_b = h_or_b5;
-             index = 23;
+             index = index_5;
              start = millis();
              
              break;
@@ -262,11 +306,11 @@ void loop(){
              lcd.setCursor(0,1);            
              lcd.print("00:00");
              mode = btnDOWN;
-             ctdwn = 3*60 + 5 + 1;
+             ctdwn = 200 + 5 ; //index_3-1 
              sound_on = false;
              sch = sch_3;
              h_or_b = h_or_b3;
-             index = 19;
+             index = index_3;
              start = millis();
  
              break;
